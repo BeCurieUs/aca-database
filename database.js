300763733d65d51f09f4d20c52fd256ef52ccdae
Christopher Willis <willis.christopher.a@gmail.com>
2019-02-05 19:34:15 -0600

    find thingy

--- database.js
+++ database.js
@@ -4,6 +4,12 @@ exports.create = function(type,data,doneCallback){
   
 }
 exports.find = function(type,id,doneCallback){
-
+  fs.readFile('db.json', (err, data) => {
+    const createdDatabase = JSON.parse(data)
+    const foundObject = createdDatabase[type].find{ item => {
+      return item["id"]==id
+    }}
+    doneCallback(foundObject)
+  });
 }
 
